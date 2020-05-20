da1a8f04ca75ded7e5d3469372a756a0ce645a7c
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-20 16:07:54 -0400

    refactored config to its own struct with a constructor for referenicng the data

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,27 @@ fn main() {
     // Cargo argument logic
     let args: Vec<String> = env::args().collect();
 
-    let query: &str = &args[1];
-    let filename: &str = &args[2];
-
-    println!("Searching for: {}", query);
-    println!("In file: {}", filename);
+    let config = Config::new(&args);
+    
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
 
     // File operation
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading file");
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading file");
     
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }
 }
