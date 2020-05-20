46ccddaa38a19e06bd2a5be9ed03ede1a6358472
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-20 16:37:36 -0400

    refactored to an independent run function to handle file contents, called with if let since we don't need to unwrap the result

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     // Cargo argument logic
@@ -16,10 +17,11 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.filename);
 
-    // File operation
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading file");
-    
-    println!("With text:\n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -37,4 +39,11 @@ impl Config {
     
         Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
