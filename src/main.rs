efd870518ed7962a7697f869df7407d1ae37a4f3
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-20 15:38:11 -0400

    can read the contents of a file in memory given a cargo run arg for filename

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // Cargo argument logic
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let query: &str = &args[1];
+    let filename: &str = &args[2];
 
     println!("Searching for: {}", query);
     println!("In file: {}", filename);
 
+    // File operation
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading file");
+    
+    println!("With text:\n{}", contents);
 }
