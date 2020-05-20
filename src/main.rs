90aeabae7f65fdaf2025ca59d92fd9d918cda5d9
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-20 16:16:27 -0400

    using Result<T, E> to handle errors more gracefully

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // Cargo argument logic
     let args: Vec<String> = env::args().collect();
+    // config Result unwrapping with closure to handle Err
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let config = Config::new(&args);
+
     
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.filename);
@@ -22,10 +28,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments supplied");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
